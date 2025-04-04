4e6f3f2fc2a9262cc71c4d8941da8c36e7ed22de
dillon-cleaver <dillon.cleaver@gmail.com>
2025-04-03 21:31:25 -0500

    feat: update app fonts to use bitter fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "Bitter-Regular": require("../assets/fonts/Bitter-Regular.ttf"),
+    "Bitter-Bold": require("../assets/fonts/Bitter-Bold.ttf"),
+    "Bitter-Italic": require("../assets/fonts/Bitter-Italic.ttf"),
+    "Bitter-BoldItalic": require("../assets/fonts/Bitter-BoldItalic.ttf"),
+    "Bitter-Medium": require("../assets/fonts/Bitter-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
