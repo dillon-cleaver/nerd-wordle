71562eae25d36ca84a960d04156f697f23d5353c
dillon-cleaver <dillon.cleaver@gmail.com>
2025-09-12 09:09:58 -0500

    feat: optimize bundle size by excluding words.json
    - Configure Metro bundler to exclude words.json from app bundle
    - Remove lodash dependency in favor of internal sample utility
    - Reduces initial app bundle size significantly

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,21 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("@expo/metro-config");
+// metro.config.js - Updated to exclude words.json from client bundles
 
-/** @type {import('expo/metro-config').MetroConfig} */
+const { getDefaultConfig } = require("expo/metro-config");
+
 const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push("cjs");
-config.resolver.unstable_enablePackageExports = false;
+
+// Exclude words.json from client bundles (it's served via CDN)
+config.resolver.blockList = [
+  ...config.resolver.blockList,
+  /constants\/words\.json$/,
+];
+
+// Allow words.json for server/build-time usage but exclude from client runtime
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
 
 module.exports = config;
