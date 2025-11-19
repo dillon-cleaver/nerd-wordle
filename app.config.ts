51398f532af45fbd2fcf02b620ff98c0a3819bd4
dillon-cleaver <dillon.cleaver@gmail.com>
2025-11-18 22:47:07 -0600

    fix: suppress dotenv logs and use correct .env.local path

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 // app.config.ts
 import * as dotenv from "dotenv";
-dotenv.config({ path: ".env" }); // change to '.env.local' if that's what you use
+dotenv.config({ path: ".env.local", quiet: true });
 
 export default {
   expo: {
